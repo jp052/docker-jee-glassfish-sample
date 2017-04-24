1beebcb9d5c51179787228daaccd47ecb64dcc92
plank <jan.plank@sulzer.de>
2017-04-24 14:25:39 +0200

    ignore removed

---
+++ src/main/java/de/sulzer/HelloWorldResource.java
@@ -0,0 +1,13 @@
+package main.java.de.sulzer;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("test")
+public class HelloWorldResource {
+    @GET
+    public String show() {
+        return "Hello World 1!";
+    }
+}
